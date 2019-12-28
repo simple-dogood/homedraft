c9c35ba12264cc2dfd9cbaaa43d8df25f8def1fc
Brian Dougherty <briandougherty@Brians-MacBook-Air.local>
2019-12-28 14:04:39 -0500

    db fail test

--- homedraft/settings.py
+++ homedraft/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'homedraft.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'home_draft',
-        'USER': 'postgres',
-        'PASSWORD':'Silence123',
-        'HOST':'localhost',
-        'PORT':'5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'home_draft',
+#         'USER': 'postgres',
+#         'PASSWORD':'Silence123',
+#         'HOST':'localhost',
+#         'PORT':'5432',
+#     }
+# }
 
 
 # Password validation
